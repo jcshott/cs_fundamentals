e0c77aefef4e996ddcdb40c6714401c42f423139
Corey Shott <corey.shott@adroll.com>
2017-03-10 09:15:21 -0800

    passed first test - basic ops

--- c_lessons/linked_lists/linked_list_homework.c
+++ c_lessons/linked_lists/linked_list_homework.c
@@ -26,16 +26,35 @@ Node *insertNode(int value, Node *head) {
     }
     // otherwise, start at head
     current = head;
-    // iterate thru list until you find the next is NULL (last node)
+    // iterate thru list until you find the last node, which is when next is NULL 
+    // re-assigning current as you go through the list
     while (current->next != NULL) {
         current = current->next;
     }
+    // when you get to last one, assign the next to the newNode
     current->next = newNode;
 
     return head;
 }
 
 Node *deleteNode(int value, Node *head) {
+    Node *current = head;
+    Node *previous = head;
+    // while we are still on a valid node
+    while (current != NULL) {
+        // if we've found our target to delete
+        if (getValFromNode(current) == value) {
+            // move our pointer from previous to current's next
+            // so that nothing is pointing to current and something is pointing to current's next
+            previous->next = current->next;
+            free(current);
+            break;
+        } else {
+             // if we haven't found target val, move current & previous along list
+            previous = current;
+            current = current->next;
+        }
+    }
 
     return head;
 }
@@ -112,33 +131,37 @@ int main(int argc, char *argv[]) {
         passed = 0;
     }
     if (passed == 1) {
-        printf("You passed the first test (basic ops)!");
+        printf("You passed the first test (basic ops)!\n");
     }
     passed = 1;
 
-    head1 = insertOrderedNode(5, head1);
-    head1 = insertOrderedNode(7, head1);
-    head1 = insertOrderedNode(1, head1);
-    if(length(head1) != 3) {
+    // delete nodes too set-up 2nd test case
+    // head1 = deleteNode(5, head1);
+    // head1 = deleteNode(1, head1);
+
+    head2 = insertOrderedNode(5, head2);
+    head2 = insertOrderedNode(7, head2);
+    head2 = insertOrderedNode(1, head2);
+    if(length(head2) != 3) {
         passed = 0;
     }
-    if(firstVal(head1) != 1) {
+    if(firstVal(head2) != 1) {
         passed = 0;
     }
-    if(lastVal(head1) != 7) {
+    if(lastVal(head2) != 7) {
         passed = 0;
     }
-    if(getMaxVal(head1) != 7) {
+    if(getMaxVal(head2) != 7) {
         passed = 0;
     }
-    head1 = deleteNode(7, head1);
-    if(firstVal(head1) != 1) {
+    head2 = deleteNode(7, head2);
+    if(firstVal(head2) != 1) {
         passed = 0;
     }
-    if(lastVal(head1) != 5) {
+    if(lastVal(head2) != 5) {
         passed = 0;
     }
-    if(getMaxVal(head1) != 5) {
+    if(getMaxVal(head2) != 5) {
         passed = 0;
     }
     if (passed == 1) {
